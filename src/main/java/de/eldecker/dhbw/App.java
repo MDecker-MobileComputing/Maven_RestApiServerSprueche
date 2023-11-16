fd3f22b8f756437f513eff213ff5c576610342c8
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-11-16 13:45:42 +0100

    Als Eclipse-Projekt importiert

--- src/main/java/de/eldecker/dhbw/App.java
+++ src/main/java/de/eldecker/dhbw/App.java
@@ -1,13 +1,9 @@
 package de.eldecker.dhbw;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	
+    public static void main( String[] args ) {
+    
+        System.out.println( "Hallo Maven!" );
     }
 }
