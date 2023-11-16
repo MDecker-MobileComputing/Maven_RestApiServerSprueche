6df186c6a53a6adffaaa740c4a33cf5604e7a0a4
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-11-16 14:36:27 +0100

    Servlet laut Vogella funktioniert

--- src/main/java/de/eldecker/dhbw/App.java
+++ src/main/java/de/eldecker/dhbw/App.java
@@ -1,9 +1,28 @@
 package de.eldecker.dhbw;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+
+/**
+ * Einstiegsklasse des Programms, startet Webserver.
+ */
 public class App {
 	
-    public static void main( String[] args ) {
+	public static final int PORT_NUMMER = 8080;
+	
+    public static void main(String[] args) throws Exception {
     
-        System.out.println( "Hallo Maven!" );
+        Server server = new Server(PORT_NUMMER);
+
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+
+        handler.addServletWithMapping(SpruecheServlet.class, "/*");
+        
+        System.out.println("Webserver wird auf Port " + PORT_NUMMER + " gestartet.");
+        server.start();
+        server.join();                
     }
 }
