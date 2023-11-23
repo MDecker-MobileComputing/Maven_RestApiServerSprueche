7cfd550ba0008a9bf8b65a1937ba481e58ae8a57
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-11-23 17:39:25 +0100

    Update SpruecheServletTest.java

--- src/test/java/de/eldecker/dhbw/test/SpruecheServletTest.java
+++ src/test/java/de/eldecker/dhbw/test/SpruecheServletTest.java
@@ -18,54 +18,54 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class SpruecheServletTest {
 
-    /** CUT: Class/Code unter Test. */ 
-    private SpruecheServlet _cut = new SpruecheServlet(); 
-    
+    /** CUT: Class/Code unter Test. */
+    private SpruecheServlet _cut = new SpruecheServlet();
+
     @Test
     void keineUrlParameter() throws Exception {
-        
+
         HttpServletRequest  requestMock  = Mockito.mock(HttpServletRequest.class);
         HttpServletResponse responseMock = Mockito.mock(HttpServletResponse.class);
-        
+
         StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
-        
+
         Mockito.when(responseMock.getWriter()).thenReturn(printWriter);
-        
+
         // *** Call method under Test ***
-        _cut.doGet(requestMock, responseMock);         
-                        
+        _cut.doGet(requestMock, responseMock);
+
         Mockito.verify(responseMock, times(1)).setStatus(400); // Bad Request
         Mockito.verify(responseMock, times(1)).setContentType("application/json");
-        
+
         String payloadResponseStr = stringWriter.toString();
         JSONObject jsonObj = new JSONObject(payloadResponseStr);
         assertFalse( jsonObj.getBoolean("erfolg") );
-        assertFalse( jsonObj.getString("text").isBlank());        
+        assertFalse( jsonObj.getString("text").isBlank());
     }
-    
+
     @Test
     void happyPath() throws Exception {
-        
+
         HttpServletRequest requestMock  = Mockito.mock(HttpServletRequest.class);
         Mockito.when(requestMock.getParameter("nummer")).thenReturn("1");
         Mockito.when(requestMock.getParameter("kategorie")).thenReturn("INFO");
-        
-        HttpServletResponse responseMock = Mockito.mock(HttpServletResponse.class);        
+
+        HttpServletResponse responseMock = Mockito.mock(HttpServletResponse.class);
         StringWriter stringWriter = new StringWriter();
-        PrintWriter printWriter = new PrintWriter(stringWriter);        
+        PrintWriter printWriter = new PrintWriter(stringWriter);
         Mockito.when(responseMock.getWriter()).thenReturn(printWriter);
-        
+
         // *** Call method under Test ***
-        _cut.doGet(requestMock, responseMock);         
-                        
+        _cut.doGet(requestMock, responseMock);
+
         Mockito.verify(responseMock, times(1)).setStatus(200); // OK
         Mockito.verify(responseMock, times(1)).setContentType("application/json");
-        
+
         String payloadResponseStr = stringWriter.toString();
         JSONObject jsonObj = new JSONObject(payloadResponseStr);
         assertTrue( jsonObj.getBoolean("erfolg") );
-        assertEquals( "Kein Backup? Kein Mitleid!", jsonObj.get("text"));       
+        assertEquals( "Kein Backup? Kein Mitleid!", jsonObj.get("text"));
     }
-    
+
 }
